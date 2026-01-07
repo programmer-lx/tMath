11bc7499473bee3af5600ad5ad4c27c7a75cda98
flx <lixing-feng@outlook.com>
2026-01-07 17:53:47 +0800

    增加失败测试

---
+++ tests/tSimd/batch/dyn_dispatch_float/not_support_instruction.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <format>
+
+#include <tSimd/batch.hpp>
+
+#include "../test.hpp"
+
+#if defined(TSIMD_COMPILER_GCC) || defined(TSIMD_COMPILER_CLANG)
+    #define TEST_FUNC_ATTR __attribute__((target("avx512f")))
+#else
+    #define TEST_FUNC_ATTR
+#endif
+
+TEST_FUNC_ATTR void call_avx512f()
+{
+    __m512 x = _mm512_set1_ps(1.0f);
+
+    const volatile void* ptr = &x;
+    std::cout << reinterpret_cast<uintptr_t>(ptr) << " ";
+
+    float sum = _mm512_reduce_add_ps(x);
+    std::cout << sum << std::endl;
+}
+
+void call_scalar()
+{
+    float x = 1;
+    const volatile float* ptr = &x;
+    std::cout << (*ptr) << std::endl;
+}
+
+using PFN = void(*)();
+static inline PFN fn_arr[] = {
+    call_scalar,
+    call_avx512f
+};
+
+TEST(not_support, avx512_failed)
+{
+    const auto& result = tsimd::InstructionSelector::get_support_info();
+    EXPECT_FALSE(result.AVX512_F); // must be failed
+
+    int idx = -1;
+    // select in runtime
+    if (result.AVX512_F)
+    {
+        idx = 1;
+    }
+    else
+    {
+        idx = 0;
+    }
+
+    // call test
+    auto fn = fn_arr[idx];
+    fn();
+
+    fn_arr[idx](); // also test this
+}
+
+int main(int argc, char **argv)
+{
+    printf("Running main() from %s\n", __FILE__);
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
